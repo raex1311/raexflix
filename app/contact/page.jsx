5d5ff2411c868caac01b2cbb6612d0a70aff2558
raex1311 <gfx460@gmail.com>
2023-12-23 16:38:55 +0530

    changes in contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -4,31 +4,31 @@ import {Image} from '@nextui-org/react';
 const contact = () => {
   return (
     <div className='bg-gray-900 min-h-screen text-white'>
-        <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto flex flex-col">
-    <div class="lg:w-3/6 mx-auto">
-      <div class="rounded-lg h-64 overflow-hidden">
-        <Image alt="content" class="object-cover object-center h-full w-full" src="/images/anime.jpg"/>
+        <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto flex flex-col">
+    <div className="lg:w-3/6 mx-auto">
+      <div className="rounded-lg h-64 overflow-hidden">
+        <Image alt="content" className="object-cover object-center h-full w-full" src="/images/anime.jpg"/>
       </div>
-      <div class="flex flex-col sm:flex-row mt-10">
-        <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-          <div class="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10" viewBox="0 0 24 24">
+      <div className="flex flex-col sm:flex-row mt-10">
+        <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+          <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10" viewBox="0 0 24 24">
               <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
               <circle cx="12" cy="7" r="4"></circle>
             </svg>
           </div>
-          <div class="flex flex-col items-center text-center justify-center">
-            <h2 class="font-medium title-font mt-4 text-lg text-gray-500">Mayuresh Rajwar</h2>
-            <div class="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
-            <p class="text-base">Email : gfx460@gmail.com</p>
-            <p class="text-base">phone : 8116867877</p>
+          <div className="flex flex-col items-center text-center justify-center">
+            <h2 className="font-medium title-font mt-4 text-lg text-gray-500">Mayuresh Rajwar</h2>
+            <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
+            <p className="text-base">Email : gfx460@gmail.com</p>
+            <p className="text-base">phone : 8116867877</p>
           </div>
         </div>
-        <div class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-          <p class="leading-relaxed text-lg mb-4">I cant tell you what it really is. I can only tell you what it feels like. And right now theres a steel knife in my wind pipe. I cant breathe but I still fight while i can fight. As long as the wrong feels right its like I am in flight.</p>
-          <a class="text-indigo-500 inline-flex items-center">Learn More
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+        <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+          <p className="leading-relaxed text-lg mb-4">I cant tell you what it really is. I can only tell you what it feels like. And right now theres a steel knife in my wind pipe. I cant breathe but I still fight while i can fight. As long as the wrong feels right its like I am in flight.</p>
+          <a className="text-indigo-500 inline-flex items-center">Learn More
+            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
               <path d="M5 12h14M12 5l7 7-7 7"></path>
             </svg>
           </a>
