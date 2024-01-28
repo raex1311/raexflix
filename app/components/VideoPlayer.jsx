e035e5c12173c88e3d268c1e2f6f43abac5928ed
raex1311 <gfx460@gmail.com>
2024-01-28 09:22:42 +0530

    Fix for subtitles

--- app/components/VideoPlayer.jsx
+++ app/components/VideoPlayer.jsx
@@ -19,7 +19,7 @@ const VideoPlayer = (theurl, subtitleURL) => {
           {
             file:{
               attributes: {
-                crossOrigin: "true"
+                crossOrigin: 'anonymous'
               },
               tracks:[{
                 kind: "subtitles",
