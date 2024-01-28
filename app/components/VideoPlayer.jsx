97ee2e5ac52af7e52f2781ed87202320924d8aed
raex1311 <gfx460@gmail.com>
2024-01-28 10:16:44 +0530

    cors_true

--- app/components/VideoPlayer.jsx
+++ app/components/VideoPlayer.jsx
@@ -19,7 +19,7 @@ const VideoPlayer = (theurl, subtitleURL) => {
           {
             file:{
               attributes: {
-                crossOrigin: "anonymous"
+                crossOrigin: true
               },
               tracks:[{
                 kind: "subtitles",
